da5b81e05092f665756fc646e9c940b13606d219
AEX <139301734+AEXdes@users.noreply.github.com>
2024-06-14 17:11:13 -0400

    [UPG]: TODO localstorage

--- app/todo.js
+++ app/todo.js
@@ -4,6 +4,22 @@ const TODO_LIST = document.querySelector(".todo__list");
 const TODO_CLOSE = document.querySelector(".todo__close");
 const TODO_CHECK = document.querySelector(".todo__check");
 
+const todoList = [];
+
+document.addEventListener("DOMContentLoaded", loadTodoListDB());
+
+function loadTodoListDB() {
+  
+  const previewTodo = localStorage.getItem("todoDB");
+  const splitPreviewTodo = previewTodo.split(",");
+
+  splitPreviewTodo.forEach((i) => {
+    if (i !== "") {
+      formatNewTask(i);     
+    }
+  })
+}
+
 function formatNewTask(newTask) {
   // Formatting new task
   const TODO_LI = document.createElement("li");
@@ -17,12 +33,23 @@ function formatNewTask(newTask) {
   TODO_TEXT.classList.add("todo__text");
   TODO_TEXT.textContent = newTask;
 
+  // Add to todoListDB
+  todoList.push(newTask);
+
   const TODO_CHECK = document.createElement("button");
   TODO_CHECK.classList.add("todo__check");
   TODO_CHECK.textContent = "✅";
 
   // Close event
   TODO_CLOSE.addEventListener("click", (e) => {
+    // Delete todo in todoListDB
+    let todoElement = e.target.parentNode.querySelector("p");
+    deleteTodoInDB(todoElement.textContent);
+
+    // Refresh LocalStorage
+    localStorage.setItem("todoDB", todoList);
+
+    // Delete todo
     e.target.parentNode.remove();
   });
 
@@ -36,6 +63,9 @@ function formatNewTask(newTask) {
   // Display task
   TODO_LI.append(TODO_CLOSE, TODO_TEXT, TODO_CHECK);
   TODO_LIST.appendChild(TODO_LI);
+
+  //Refresh LocalStorage
+  localStorage.setItem("todoDB", todoList);
 }
 
 TODO_ADD.addEventListener("click", () => {
@@ -52,3 +82,12 @@ TODO_WRITE.addEventListener("keydown", (e) => {
     TODO_WRITE.value = "";
   }
 });
+
+function deleteTodoInDB(todoText) {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i] === todoText) {
+      todoList.splice(i, 1);
+      break;
+    }
+  }
+}
